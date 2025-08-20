4ffb599dd46f9f9633d9cc4fb8bd14c295da8398
Sk.Lackmul Haque(Nazmul) <lackmulbe@gmail.com>
2025-08-20 11:38:52 +0600

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), // React plugin (JSX, Fast Refresh, etc.)
-  ],
+  plugins: [react(),  tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // @ = src shortcut
